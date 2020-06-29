b11c0a61ae84bf0ee88c172153498123b7742ccc
Ebrahim Aharpour <ebrahim.aharpour@gmail.com>
2020-06-29 18:52:11 +0200

    Adding a production env.

---
+++ environments/prod/variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+applicationName="theapp"
+namespace=myprod
+deployment=mydeployment
+volumeMultiplicityPerNode=1
+# volumes=("repository" "logs" "database")
+# volumesRootFolder="/var/lib/k8s"
